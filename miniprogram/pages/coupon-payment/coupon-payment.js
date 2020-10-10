9e7e70bc4bf057ac5fe373b6c01984e69120e54b
uilibicheng <35124429+uilibicheng@users.noreply.github.com>
2020-10-10 17:06:13 +0800

    完成优惠券付款页面

--- miniprogram/pages/coupon-payment/coupon-payment.js
+++ miniprogram/pages/coupon-payment/coupon-payment.js
@@ -6,13 +6,26 @@ Page({
   data: {
     couponInfo: {},
     merchantInfo: {},
+    count: 1,
+    phone: '',
+    bindPhone: '',
   },
 
   onLoad(params) {
-    console.log(11, params)
+    this.getCurrentUser()
     if (params.id) {
       this.getCouponDetail(params.id)
     }
+  },
+
+  getCurrentUser() {
+    wx.BaaS.auth.getCurrentUser().then(user => {
+      let phone = user._attribute._phone || ''
+      this.setData({
+        phone,
+        bindPhone: phone,
+      })
+    })
   },
 
   getCouponDetail(id) {
@@ -29,4 +42,62 @@ Page({
       })
     })
   },
+
+  handleInput(e) {
+    this.setData({
+      bindPhone: e.detail.value
+    })
+  },
+
+  handleBuy() {
+    if (!this.data.bindPhone) {
+      wx.showToast({
+        title: `请输入手机号码`,
+        duration: 2000,
+        icon: 'none'
+      })
+      return
+    }
+    if (!this.data.phone) {
+      wx.BaaS.auth.getCurrentUser()
+        .then(user => {
+          return user.setMobilePhone(this.data.bindPhone)
+        })
+    }
+    wx.showToast({
+      title: `支付功能尚未开通，敬请期待`,
+      duration: 2000,
+      icon: 'none'
+    })
+  },
+
+  changeCount(e) {
+    let type = e.currentTarget.dataset.type
+    let count = this.data.count
+    let limit_buy_count = this.data.couponInfo.limit_buy_count
+    if (type === 'add') {
+      if (count >= limit_buy_count) {
+        wx.showToast({
+          title: `可购买数量为${limit_buy_count}张`,
+          duration: 2000,
+          icon: 'none'
+        })
+        return
+      }
+      count += 1
+    } else {
+      if (count <= 1) {
+        wx.showToast({
+          title: `最少购买1张`,
+          duration: 2000,
+          icon: 'none'
+        })
+        return
+      }
+      count -= 1
+    }
+    this.setData({
+      count,
+    })
+  }
 })
